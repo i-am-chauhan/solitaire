9670f499c4271069fe0423d3c2857829c53ffc33
shubhiamchauhan <shubham25may1998@gmail.com>
2019-04-14 15:26:03 +0530

    Updates

---
+++ precache-manifest.4c6fd4554cb02b89921545e15d6d2a95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6266f8bb4d122008df",
+    "url": "/solitaire/static/css/main.2bf47781.chunk.css"
+  },
+  {
+    "revision": "0e6266f8bb4d122008df",
+    "url": "/solitaire/static/js/main.5b56ee6a.chunk.js"
+  },
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "5f16c39ea275cb26fb4e",
+    "url": "/solitaire/static/js/2.a60ebcec.chunk.js"
+  },
+  {
+    "revision": "5fc3106e76a8ca27ab713a55a650d85c",
+    "url": "/solitaire/index.html"
+  }
+];
